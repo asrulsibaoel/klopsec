c6fcd3d88f9d275698e8782ae1dac4006f40275f
asrul sani ariesandy <asrul.sani@koinworks.com>
2023-04-10 03:42:08 +0000

    Ignored: istio file sources

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-gcloud container clusters get-credentials $CLUSTER_NAME --region $GCP_REGION --project $PROJECT_ID
+gcloud container clusters get-credentials $CLUSTER_NAME --zone $GCP_REGION --project $PROJECT_ID
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=$ISTIO_VERSION TARGET_ARCH=x86_64 sh -
+
+cd istio-$ISTIO_VERSION && export PATH=$PWD/bin:$PATH
+istioctl install --set profile=demo -y
+kubectl label namespace default istio-injection=enabled
+
 kubectl create namespace seldon-system
 
 helm install seldon-core seldon-core-operator \
@@ -9,5 +15,4 @@ helm install seldon-core seldon-core-operator \
     --set istio.enabled=true
 
 kubectl create namespace seldon
-
-
+kubectl create namespace mlflow
